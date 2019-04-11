8822c10305aad63c414a7c4881d3683336ffc229
Chanda Grayson <chandagrayson@gmail.com>
2019-04-11 16:58:02 -0400

    beginning semantic

--- src/Components/PolishCard.js
+++ src/Components/PolishCard.js
@@ -1,4 +1,25 @@
 import React from 'react'
+import { Card } from 'semantic-ui-react'
 
+const PolishCard = () => {
 
-// export default PolishCard
+    return (
+        <div className="card">
+        <Card.Group>
+           <Card color='pink'>
+               {/* <Image src='' /> */}
+               <Card.Content>
+                   <Card.Header>Polish Name</Card.Header>
+                   <Card.Meta>Other stuff</Card.Meta>
+                   <Card.Description>Polish Info</Card.Description>
+               </Card.Content>
+           </Card>
+           </Card.Group>
+            {/* <h2>{props.polish.name}</h2> */}
+            {/* <img className="polish-img" src{props.polish.image} /> */}
+
+        </div>
+    )
+}
+
+export default PolishCard
--- src/Components/PolishContainer.js
+++ src/Components/PolishContainer.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import PolishCard from './PolishCard.js'
+import React, { Component } from 'react'
+import PolishCard from './PolishCard'
 import PolishFavorites from './PolishFavorites'
 
 class PolishContainer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            polishes: []
+        }
+    }
+
 
     render() {
         return(
             <div>
-                
+                <p></p>
             </div>
         )
     }
 
 }
-
 
 
 
