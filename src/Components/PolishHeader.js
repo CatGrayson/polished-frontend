4dc83c507e68595b0ea17d42782ed130299b97bb
Megan Fago <meganlaynefago@gmail.com>
2019-04-11 16:59:27 -0400

    Began route situation

--- src/Components/PolishHeader.js
+++ src/Components/PolishHeader.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import PolishLogin from './PolishLogin.js'
-import PolishSort from './PolishSort.js'
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import PolishLogin from './PolishLogin.js';
+// import PolishSort from './PolishSort.js';
 
 
 const PolishHeader = (props) => {
     return(
-        <div>
-            <h1>POLISHED</h1>
+        <div className="header">
+        {/* <Link to="/users" className="header"> */}
+        
+            <h1>P O L I S H E D</h1>
+    
+        {/* </Link> */}
         </div>
     )
 }
