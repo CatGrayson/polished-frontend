5c00fc27d692074d100520fd8e38ff06d10508e9
Megan Fago <meganlaynefago@gmail.com>
2019-04-15 18:32:34 -0400

    + FavoriteCard & tried 2 flip the card back SOS

--- src/Components/PolishFavorites.js
+++ src/Components/PolishFavorites.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import FavoriteCard from './FavoriteCard';
 
 class PolishFavorites extends Component {
     render(){
         return(
             <div>
                 <h2>Faves</h2>
+                {this.props.favorites.map(polish => <FavoriteCard polish={polish}/>)}
             </div>
         )
     }
